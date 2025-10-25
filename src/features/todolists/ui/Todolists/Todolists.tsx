7764e4d319e910900b43d754d137f842ce51a5ee
BohdanKrav4enko <bohdan.krav4enko@gmail.com>
2025-10-25 18:46:24 +0300

    chore: мелкие изменения

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -25,7 +25,7 @@ export const Todolists = () => {
     <>
       {todolists?.map((todolist) => (
         <Grid key={todolist.id}>
-          <Paper sx={{ p: "0 20px 20px 20px" }}>
+          <Paper sx={{ p: "0 20px 20px 20px", m: "20px"}}>
             <TodolistItem todolist={todolist} />
           </Paper>
         </Grid>
